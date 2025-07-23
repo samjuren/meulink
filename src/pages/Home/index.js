b34cf05b845b761ad6e852ba32c8a20fc15fea24
Samuel Juren <sammjureen@outlook.com>
2025-07-23 17:58:12 -0300

    home - animações

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,21 @@
+import { FiLink } from "react-icons/fi";
+import "./home.css";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Página Home</h1>
+    <div className="conteiner-home">
+      <div className="logo">
+        <img src="/logo.png" alt="Sujeito Link Logo" />
+        <h1>Meu Link</h1>
+        <span>Compartilhe seus links facilmente!</span>
+      </div>
+      <div className="area-input">
+        <div>
+          <FiLink size={24} color="#FFF" />
+          <input placeholder="Cole seu link aqui..." />
+        </div>
+        <button>Gerar Link</button>
+      </div>
     </div>
   );
 }
