bda65a40b188f7581e3df2953e0060d39b56cdb1
Samuel Juren <sammjureen@outlook.com>
2025-07-23 17:33:04 -0300

    routeamento react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { RoutesApp } from "./routes";
+
 export default function App() {
   return (
     <div className="app">
-      <h1>Sujeito Link 123</h1>
+      <RoutesApp />
     </div>
   );
 }
---
+++ src/pages/Links/index.js
@@ -0,0 +1,7 @@
+export default function Links() {
+  return (
+    <div>
+      <h1>Página Meus Links</h1>
+    </div>
+  );
+}
