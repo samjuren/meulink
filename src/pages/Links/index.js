6f1ee5ce7bbf904c87b3b03276a46fb04651f8b7
Samuel Juren <sammjureen@outlook.com>
2025-07-25 21:46:54 -0300

    links

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -3,8 +3,31 @@ import "./links.css";
 
 export default function Links() {
   return (
-    <div>
-      <h1>Página Meus Links</h1>
+    <div className="links-container">
+      <div className="links-header">
+        <FiArrowLeft size={38} color="#FFF" />
+        <h1>Meus Links</h1>
+      </div>
+
+      <div className="links-item">
+        <button className="link">
+          <FiLink size={18} color="#FFF" />
+          https://sujeitoprogramador.com
+        </button>
+        <button className="link-delete">
+          <FiTrash size={24} color="#FF5454" />
+        </button>
+      </div>
+
+      <div className="links-item">
+        <button className="link">
+          <FiLink size={18} color="#FFF" />
+          https://sujeitoprogramador.com
+        </button>
+        <button className="link-delete">
+          <FiTrash size={24} color="#FF5454" />
+        </button>
+      </div>
     </div>
   );
 }
